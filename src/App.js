914134db38420678cd900b41fc435edcbcb005ea
Adityanarayan Tiwari <adityant17121997@gmail.com>
2022-03-13 22:38:01 +0530

    Layout Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Header from './components/Header/Header';
-
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import {CssBaseline, Grid} from '@material-ui/core'
 const App = () => {
   return (
-    <div>
+    <>
+    <CssBaseline />
       <Header />
-    </div>
+      <Grid container spacing={3} style={{width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
